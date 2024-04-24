53a677960646a30a425f3c253a49c6591826a51b
mith10 <mithilesh.rajput21@pccoepune.org>
2024-04-24 16:47:12 +0530

    php html and mysql connection

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+ 
+// form submitted by index.html take values from it and store in variables 
+
+
+
+
+// connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// insert values into the database
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('$firstname', '$lastname', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?
